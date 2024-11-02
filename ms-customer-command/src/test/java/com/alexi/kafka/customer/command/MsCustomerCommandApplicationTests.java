90692649cc9bd352ea07a0e0e8520195a49702c0
alexi-ae <alexi.acunaespino@gmail.com>
2024-11-02 15:14:41 -0500

    feat - add ms-customer-command

---
+++ ms-customer-command/src/test/java/com/alexi/kafka/customer/command/MsCustomerCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexi.kafka.customer.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

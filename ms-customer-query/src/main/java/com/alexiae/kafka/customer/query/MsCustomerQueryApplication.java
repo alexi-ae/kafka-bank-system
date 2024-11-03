25f9ceb2d2a17ae9400ae075b964f41ccb9f3c1d
alexi-ae <alexi.acunaespino@gmail.com>
2024-11-03 15:27:24 -0500

    feat - ms customer query

---
+++ ms-customer-query/src/main/java/com/alexiae/kafka/customer/query/MsCustomerQueryApplication.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka.customer.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomerQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCustomerQueryApplication.class, args);
+    }
+
+}
---
+++ ms-customer-query/src/test/java/com/alexiae/kafka/customer/query/MsCustomerQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka.customer.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomerQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

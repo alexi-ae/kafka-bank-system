1575d999826803c3df9d95fc49e2f984f7236be0
alexi-ae <alexi.acunaespino@gmail.com>
2024-11-03 23:32:56 -0500

    feat - auth command

---
+++ auth-command/src/main/java/com/alexiae/kafka/auth/AuthCommandApplication.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthCommandApplication.class, args);
+    }
+
+}

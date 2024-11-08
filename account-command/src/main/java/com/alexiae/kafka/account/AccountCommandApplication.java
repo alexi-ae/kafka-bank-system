9f5064e6db503cf893609ba4665b33cc497a2420
alexi-ae <alexi.acunaespino@gmail.com>
2024-11-08 13:37:10 -0500

    feat - add ms account

---
+++ account-command/src/main/java/com/alexiae/kafka/account/AccountCommandApplication.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountCommandApplication.class, args);
+    }
+
+}
---
+++ account-command/src/test/java/com/alexiae/kafka/account/AccountCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

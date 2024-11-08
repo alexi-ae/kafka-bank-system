71f1425bafe3cffd5d355b4295b9ed8faf1fdc63
alexi-ae <alexi.acunaespino@gmail.com>
2024-11-08 16:05:22 -0500

    feat - add config security

---
+++ account-command/src/main/java/com/alexiae/kafka/account/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.alexiae.kafka.account.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private static final String[] WHITE_LIST_URL = {
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
+            "/auth/**",
+            "/ob/register",
+            "/h2-console",
+            "/h2-console/**"
+
+    };
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Utiliza BCrypt para hashear contraseñas
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(
+                        AbstractHttpConfigurer::disable) // Disabling CSRF as we use JWT which is immune to CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(WHITE_LIST_URL).permitAll() // Whitelisting some paths from authentication
+                        .requestMatchers("/customer/**").hasAnyRole("CUSTOMER")
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated()) // All other requests must be authenticated
+                //.authenticationProvider(authenticationProvider)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session management
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class); // Registering our JwtAuthFilter
+        return http.build(); // Construir el filtro de seguridad
+    }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+
+}

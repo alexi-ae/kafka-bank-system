838cf14603b1c826b0ac14f6c5702a5e3806aa9f
alexi-ae <alexi.acunaespino@gmail.com>
2024-11-10 19:33:01 -0500

    feat - add transaction command

---
+++ transaction-command/src/main/java/com/alexiae/kafka/transaction/TransactionCommandApplication.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionCommandApplication.class, args);
+    }
+
+}
---
+++ transaction-command/src/test/java/com/alexiae/kafka/transaction/TransactionCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexiae.kafka.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
